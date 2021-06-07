d74caed69baf5792f1f207ac904eaa3daa4d492e
TalekC-137 <pawatl2002@gmail.com>
2021-06-07 18:02:12 +0200

    kinda working sound measuring

--- app/src/main/java/com/scollon/tapr/MainActivity.kt
+++ app/src/main/java/com/scollon/tapr/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.scollon.tapr
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Handler
+import android.util.Half.EPSILON
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -16,14 +28,23 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private val gravity = FloatArray(3)
     private val linear_acceleration = FloatArray(3)
 
+    private val NS2S = 1.0f / 1000000000.0f
+    private val deltaRotationVector = FloatArray(4) { 0f }
+    private var timestamp: Float = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         val mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val gyroSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -33,6 +54,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent) {
         // The light sensor returns a single value.
         // Many sensors return 3 values, one for each axis.
+
+
+        //linear acceleration here
         val lux = event.values[0]
 
         val alpha: Float = 0.8f
@@ -47,24 +71,97 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         linear_acceleration[1] = event.values[1] - gravity[1]   //Y m/s2
         linear_acceleration[2] = event.values[2] - gravity[2]   //Z m/s2
 
-        // Do something with this sensor value.
 
-        tvX.text = linear_acceleration[0].toString()
-        tvY.text = linear_acceleration[1].toString()
-        tvZ.text = linear_acceleration[2].toString()
+        /*
+        having every X,Y and Z acceleration will help us determine
+        whether the phone was tapper from the side, top, botton or back
+         */
+        tvX.text = "laX: " + linear_acceleration[0].toInt().toString()
+        tvY.text = "laY: " + linear_acceleration[1].toInt().toString()
+        tvZ.text = "laZ: " + linear_acceleration[2].toInt().toString()
+
+
+        //gyroscope here
+
+            // This timestep's delta rotation to be multiplied by the current rotation
+            // after computing it from the gyro sample data.
+            if (timestamp != 0f && event != null) {
+                val dT = (event.timestamp - timestamp) * NS2S
+                // Axis of the rotation sample, not normalized yet.
+                var axisX: Float = event.values[0]
+                var axisY: Float = event.values[1]
+                var axisZ: Float = event.values[2]
+
+                var axisX2: Int = axisX.toInt()
+                var axisY2: Int = axisY.toInt()
+                var axisZ2: Int = axisZ.toInt()
+
+                tvgX.text = axisX2.toString()
+                tvgY.text = axisY2.toString()
+                tvgZ.text = axisZ2.toString()
+                // Calculate the angular speed of the sample
+                val omegaMagnitude: Float = sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ)
+
+                // Normalize the rotation vector if it's big enough to get the axis
+                // (that is, EPSILON should represent your maximum allowable margin of error)
+                if (omegaMagnitude > EPSILON) {
+                    axisX /= omegaMagnitude
+                    axisY /= omegaMagnitude
+                    axisZ /= omegaMagnitude
+
+
+                }
+
+                // Integrate around this axis with the angular speed by the timestep
+                // in order to get a delta rotation from this sample over the timestep
+                // We will convert this axis-angle representation of the delta rotation
+                // into a quaternion before turning it into the rotation matrix.
+                val thetaOverTwo: Float = omegaMagnitude * dT / 2.0f
+                val sinThetaOverTwo: Float = sin(thetaOverTwo)
+                val cosThetaOverTwo: Float = cos(thetaOverTwo)
+                deltaRotationVector[0] = sinThetaOverTwo * axisX
+                deltaRotationVector[1] = sinThetaOverTwo * axisY
+                deltaRotationVector[2] = sinThetaOverTwo * axisZ
+                deltaRotationVector[3] = cosThetaOverTwo
+
+
+
+
+            }
+            timestamp = event?.timestamp?.toFloat() ?: 0f
+            val deltaRotationMatrix = FloatArray(9) { 0f }
+            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+            // User code should concatenate the delta rotation we computed with the current rotation
+            // in order to get the updated rotation.
+            // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+
+            //displaying the output
+
+               // deltaRotationVector[0] = deltaRotationVector[0] * deltaRotationMatrix
+          //  tvgX.text =
+
+
 
 
     }
 
     override fun onResume() {
+
         super.onResume()
         sensor?.also { sensor ->
             sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
         }
+
     }
 
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
+
+        super.onPause();
+
     }
+
+
 }
+
